315c08e392a99916c6fbe6a0c2d1d41a5a52c267
Иваненко Андрей Игоревич <AIgIvanenko@sberbank.ru>
2024-01-29 15:12:53 +0300

    Add request and response body type BestSaleRate

---
+++ src/main/java/com/andpostman/currencyexchange/property/BestSaleRate.java
@@ -0,0 +1,23 @@
+package com.andpostman.currencyexchange.property;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonRootName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+@Data
+@AllArgsConstructor
+@JsonRootName("best_sale_rates")
+public class BestSaleRate implements Serializable {
+    @JsonProperty("bank_name")
+    String bankName;
+    @JsonProperty("address")
+    String address;
+    @JsonProperty("buy_rate")
+    BigDecimal bayRate;
+    @JsonProperty("sell_rate")
+    BigDecimal sellRate;
+
+}
